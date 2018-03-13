38b8eb7bb81476eef6cca06d270dbc75e8750d6c
Kinshuk <kinshuk.ram@gmail.com>
2018-03-13 19:52:26 +0100

    feat: Add the network checking utility

---
+++ app/src/main/java/com/k2udacity/baking/utils/NetworkUtils.java
@@ -0,0 +1,31 @@
+package com.k2udacity.baking.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class NetworkUtils {
+    private static String TAG = "NetworkUtils";
+
+    private NetworkUtils() {
+    }
+
+    public static boolean isInternetAvailable(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+    }
+
+}
