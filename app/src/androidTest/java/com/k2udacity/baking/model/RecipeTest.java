9fea60a529ed6140f191681f5dc50fc45b9ffde7
Kinshuk <kinshuk.ram@gmail.com>
2018-03-15 16:44:02 +0100

    feat: Add the test dependency

--- app/src/androidTest/java/com/k2udacity/baking/model/RecipeTest.java
+++ app/src/androidTest/java/com/k2udacity/baking/model/RecipeTest.java
@@ -1,7 +1,7 @@
 package com.k2udacity.baking.model;
 
 import android.os.Parcel;
-import android.support.test.espresso.core.deps.guava.collect.ImmutableList;
+import android.support.test.espresso.core.internal.deps.guava.collect.ImmutableList;
 
 import org.junit.Test;
 
@@ -20,7 +20,7 @@ public class RecipeTest {
         Step step = new Step("id", "short desc", "desc", "vid_url", "thumb_url");
         Ingredient ingredient = new Ingredient("measure", "ingredient", "quantity");
 
-        Recipe recipe = new Recipe("id", "name",ImmutableList.of(ingredient), ImmutableList.of(step), "servings", "image");
+        Recipe recipe = new Recipe("id", "name", ImmutableList.of(ingredient), ImmutableList.of(step), "servings", "image");
         Parcel parcel = obtainParcelForParcelable(recipe);
 
         Recipe createdFromParcel = Recipe.CREATOR.createFromParcel(parcel);
